7ab3f2c02cca4a1c8aeace7c7ac2d49474546d28
mosidrum <mosiokanga@gmail.com>
2023-11-07 05:24:50 +0100

    add home

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import { Header } from "./components/Header";
-import Home from "./components/Home";
+import Home from "./components/Home/Home";
 
 const App = () => {
   return (
