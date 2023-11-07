b677cfc4a06f87a3a88507bea87c3679ccfce65d
Ayodele Isaac Mosiron <mosiokanga@gmail.com>
2023-11-07 05:41:09 +0100

    finish up version 1 (#3)

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import "./App.css";
 import { Header } from "./components/Header";
-import Home from "./components/Home/Home";
+import { Home } from "./components/Home";
+import { About } from "./components/About";
+import { Projects } from "./components/Projects";
+import { Contact } from "./components/Contact";
+import Footer from "./shared/Footer"
 
 const App = () => {
   return (
     <>
       <Header />
       <Home />
+      <About />
+      <Projects />
+      <Contact />
+      <Footer />
     </>
   );
 }
