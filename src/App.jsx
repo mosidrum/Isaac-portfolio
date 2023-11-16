7720552e0173045582dbb6717907ba3712ea8c6b
mosidrum <mosiokanga@gmail.com>
2023-11-16 13:18:16 +0100

    Fix vercel error

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { Header } from "./components/Header";
 import { Home } from "./components/Home";
 import { About } from "./components/About";
 import { Projects } from "./components/Projects";
-import { Contact } from "./components/Contact";
 import Footer from "./shared/Footer"
 
 const App = () => {
