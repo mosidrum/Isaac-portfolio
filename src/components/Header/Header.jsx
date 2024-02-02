41ce3f68e2f85e08af8b6e4be5196ddf32e3fac6
Ayodele Isaac Mosiron <mosiokanga@gmail.com>
2024-02-02 10:03:35 +0100

    Update frammer motion (#7)
     Update frammer motion

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,10 @@
 import {useState} from 'react';
-import isaac from '../../assets/Isaac.jpg';
 import {Link} from 'react-scroll';
 import {Bars3Icon, XMarkIcon} from '@heroicons/react/24/solid'
 import { navLinks } from '../../utils/navLinks';
 const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false);
+  const isaac = 'https://i.ibb.co/Gd8H6bQ/Isaac.jpg';
   return (
     <div className="w-full relative z-[100]">
       <div className="md:flex items-center justify-between bg-white py-4 md:py-0 md:px-14 px-7 fixed top-0 left-0 right-0 shadow-sm">
@@ -16,7 +16,7 @@ const Header = () => {
           {menuOpen ? <XMarkIcon /> : <Bars3Icon />}
         </div>
           <ul className={`md:flex md:items-center items-center md:pb-0 absolute md:static md:z-auto z-50 left-0 w-full md:w-auto mt-4 md:mt-0 md:pl-0 bg-white sm:bg-transparent ${menuOpen? 'top-12' : 'top-[-490px]'}`}>
-            {navLinks.map( link => <li key={link.name} className="md:ml-8 md:my-8 mt-5 font-semibold text-x1 border-b-2 md:border-b-0 text-left px-7 md:px-4 pb-4 md:pb-0 cursor-pointer">
+            {navLinks.map( link => <li key={link.name} className="md:ml-8 md:my-8 mt-5 font-semibold text-x1 border-b-2 md:border-b-0 text-left. px-7 md:px-4 pb-4 md:pb-0 cursor-pointer">
               <Link
               to={link.link}
               activeClass='active'
--- src/utils/navLinks.js
+++ src/utils/navLinks.js
@@ -10,9 +10,5 @@ export const navLinks = [
     {
         name: 'Projects',
         link: 'projects'
-    },
-    {
-        name: 'Contact',
-        link: 'contact'
-    },
+    }
 ]
