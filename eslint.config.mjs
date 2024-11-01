a7db9f9a848c0153e602be492d17bb4951370ba3
mosidrum <mosiokanga@gmail.com>
2024-11-01 05:28:27 +0100

    add prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,8 @@ import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 import js from '@eslint/js';
 import { FlatCompat } from '@eslint/eslintrc';
+import eslintConfigPrettier from "eslint-config-prettier";
+
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -134,5 +136,6 @@ export default [
       'react-hooks/rules-of-hooks': 'error',
       'react-hooks/exhaustive-deps': 'warn'
     }
-  }
+  },
+  eslintConfigPrettier,
 ];
