9ed67a43e9505f3c9c8905a07a4f9cb2ef7fc47d
Isaac Ayodele <mosiokanga@gmail.com>
2024-11-07 09:22:55 +0100

    add nav bar components (#9)

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,1 +1,34 @@
-export const Navbar = () => <div>Navbar</div>;
+import styles from './navbar.module.css';
+import { GotoLink } from '../gotoLink';
+
+const navItems = [
+  {
+    name: 'About me',
+    link: '/about'
+  },
+  {
+    name: 'Projects',
+    link: '/projects'
+  },
+  {
+    name: 'Blog',
+    link: '/blog'
+  },
+  {
+    name: 'Uses',
+    link: '/uses'
+  }
+];
+
+export const Navbar = () => (
+  <div className={styles.container}>
+    <div className={styles.wrapper}>
+      <h1 className={styles.title}>Isaac</h1>
+      <div className={styles.items}>
+        {navItems.map((item, index) => (
+          <GotoLink href={item.link} title={item.name} key={index} />
+        ))}
+      </div>
+    </div>
+  </div>
+);
