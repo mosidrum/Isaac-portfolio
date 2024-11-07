6f28866a27c1689c565996b8dbc3afe3cdaf6fc4
mosidrum <mosiokanga@gmail.com>
2024-11-07 20:25:09 +0100

    add hero section

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,5 +1,37 @@
+import Link from 'next/link';
+import { RiLinkedinFill } from 'react-icons/ri';
+import { FaGithub, FaLinkedinIn, FaRegEnvelope } from 'react-icons/fa';
+import styles from './hero.module.css'
+import Image from 'next/image';
+
+export const iconItems = [
+  {
+    name: 'linkedin',
+    icon: <FaLinkedinIn />,
+  },
+  {
+    name: 'github',
+    icon: <FaGithub />
+  },
+  {
+    name: 'message',
+    icon: <FaRegEnvelope />
+
+  }
+]
+
 export const Hero = () => (
   <div>
     <h2>👋 Hello I am</h2>
+    <div className={styles.title}>Isaac Ayodele</div>
+    <p>A passionate software engineer and a firm believer that technology can address humanity&apos;s most pressing challenges. My expertise lies in front-end development. I enjoy developing clean, accessible and high-performant web applications.</p>
+    <Link className="text-no-underline" href='/'>
+      <p className="text-secondary">More about me</p>
+    </Link>
+    <div className="d-flex gap-2">
+      {iconItems.map((item, index) => (
+        <div key={index}>{item.icon}</div>
+      ))}
+    </div>
   </div>
 );
--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,9 +1,9 @@
 import { navItems } from '../../utils';
-import './footer.module.css';
+import styles from './footer.module.css';
 
 export const Footer = () => (
-  <div className="footer">
-    <hr className="line" />
+  <div className={styles.footer}>
+    <hr className={styles.line} />
     <div className="flex-center-between pv-4 ph-4">
       <div className="d-flex gap-2">
         {navItems.map((item, index) => (
