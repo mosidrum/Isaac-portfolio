941ca74b7950d27883c0e5bb953c0cecb583f91a
mosidrum <mosiokanga@gmail.com>
2024-11-21 19:45:39 +0100

    quick update on experience

--- src/utils/experiences.ts
+++ src/utils/experiences.ts
@@ -1,31 +1,31 @@
-import eua from '../assests/eua.jpeg'
-import salesPlat from '../assests/salesplat.jpeg'
-import lampstand from '../assests/lampstand.png'
-import microverse from '../assests/microverse.jpeg'
+import eua from '../assests/eua.jpeg';
+import salesPlat from '../assests/salesplat.jpeg';
+import lampstand from '../assests/lampstand.png';
+import microverse from '../assests/microverse.jpeg';
 
 export const experiences = [
   {
     name: 'Lampstand',
     logo: lampstand,
     position: 'Software Engineer',
-    duration: 'April 2024 - Current',
+    duration: 'April 2024 - Current'
   },
   {
     name: 'SalesPlat',
     logo: salesPlat,
     position: 'Frontend Engineer',
-    duration: 'June 2024 - Current',
+    duration: 'June 2024 - October 2024'
   },
   {
     name: 'eUniversity Africa',
     logo: eua,
     position: 'Frontend Engineer',
-    duration: 'July 2023 - March 2024',
+    duration: 'July 2023 - March 2024'
   },
   {
     name: 'Microverse',
     logo: microverse,
     position: 'Software Engineer',
-    duration: 'March 2023 - September 2023',
+    duration: 'March 2023 - September 2023'
   }
-]
+];
